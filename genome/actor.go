a31acca10488ca8f377b464d0858b146f58b6ad6
Kirill Lastovirya <kirill.lastovirya@jobseeker.com.au>
2017-02-01 20:07:13 +1100

    added new classes for actor and receptor

---
+++ genome/actor.go
@@ -0,0 +1,4 @@
+package genome
+
+// Actor creates action in surrounding world
+type Actor interface{}
---
+++ genome/receptor.go
@@ -0,0 +1,5 @@
+package genome
+
+// Receptor accepts lore and transforms it to internal signal
+type Receptor interface {
+}
